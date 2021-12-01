c905b4112e532a4d087e6bf805094904429c5c5f
양탕구리 <rukeni@naver.com>
2021-12-01 22:16:04 +0900

    Add : create 1day project

---
+++ vanillajs/50Projects/day1/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', function(e) {
+    removeActiveClasses();
+    this.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
