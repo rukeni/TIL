3f2a03ae6b9f820a91400b2c3466dee61494310e
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-02-25 23:53:45 +0900

    Add : 무한 뺄셈

---
+++ algorithms/frontend-school/47.js
@@ -0,0 +1,31 @@
+/*** 5. 무한 뺄셈 ***/
+
+/* user code */
+function answer(s, e) {
+  let sequence = [];
+  sequence.push(s);
+  sequence.push(e);
+
+  // 코드 구현 시작 영역
+
+  // …
+
+  // 코드 구현 종료 영역
+
+  return sequence;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [9, 3],
+  // TC: 2
+  [6, 3],
+  // TC: 3
+  [13, 7],
+];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i][0], input[i][1]));
+}
