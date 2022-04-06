5eda37a69c5a84202bfdab0367d1b60edc89e8b8
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-04-06 23:58:51 +0900

    Chore : 프리티어 적용

--- algorithms/frontend-school/linear/두수최대합.js
+++ algorithms/frontend-school/linear/두수최대합.js
@@ -4,31 +4,28 @@
 // 배열로 입력되는 정수는 10 - 20개 사이이며 정수의 범위는 -20 ~ +20 사이의 값이 입력된다
 /* user code */
 function answer(nums) {
-  let result = [];
+	let result = [];
 
-  nums.sort((a,b) => b-a)
-  result.push(nums[0]);
-  result.push(nums[1])
+	nums.sort((a, b) => b - a);
+	result.push(nums[0]);
+	result.push(nums[1]);
 
-  return result;
+	return result;
 }
 
 /* main code */
 let input = [
-  // TC: 1
-  [-11, 5, 18, -2, -3, 6, 4, 17, 10, 9],
+	// TC: 1
+	[ -11, 5, 18, -2, -3, 6, 4, 17, 10, 9 ],
 
-  // TC: 2
-  [3, 7, -14, 2, -6, 13, -20, -2, -7, 6, -17, -5, 14, -9, 19],
+	// TC: 2
+	[ 3, 7, -14, 2, -6, 13, -20, -2, -7, 6, -17, -5, 14, -9, 19 ],
 
-  // TC: 3
-  [
-    -15, -4, -8, 12, 12, -8, -8, 9, 10, 15, -2, 10, -14, 2, 13, 19, -9, 3, -18,
-    14,
-  ],
+	// TC: 3
+	[ -15, -4, -8, 12, 12, -8, -8, 9, 10, 15, -2, 10, -14, 2, 13, 19, -9, 3, -18, 14 ],
 ];
 
 for (let i = 0; i < input.length; i++) {
-  process.stdout.write(`#${i + 1} `);
-  console.log(answer(input[i]));
+	process.stdout.write(`#${i + 1} `);
+	console.log(answer(input[i]));
 }
--- algorithms/frontend-school/linear/배열문제풀이_일곱난장이_문제.js
+++ algorithms/frontend-school/linear/배열문제풀이_일곱난장이_문제.js
@@ -5,33 +5,33 @@
 // 배지 값은 100이하 자연수로 들어오며 일곱 난장이의 배지 값을 기존 순서대로 배열에 넣어 반환한다
 /* user code */
 function answer(dwarf) {
-  let result = [];
+	let result = [];
 
-  const difference = dwarf.reduce((acc,curr) => acc += curr, 0) - 100;
-  for(let i = 0; i < dwarf.length; i++) {
-    const relativeNumber = Math.abs(difference - dwarf[i]);
+	const difference = dwarf.reduce((acc, curr) => (acc += curr), 0) - 100;
+	for (let i = 0; i < dwarf.length; i++) {
+		const relativeNumber = Math.abs(difference - dwarf[i]);
 
-    if(dwarf.findIndex(dwarf => dwarf === relativeNumber) === -1 || dwarf[i] > difference) {
-      result.push(dwarf[i])
-    }
-  }
+		if (dwarf.findIndex((dwarf) => dwarf === relativeNumber) === -1 || dwarf[i] > difference) {
+			result.push(dwarf[i]);
+		}
+	}
 
-  return result;
+	return result;
 }
 
 /* main code */
 let input = [
-  // TC: 1
-  [1, 5, 6, 7, 10, 12, 19, 29, 33],
+	// TC: 1
+	[ 1, 5, 6, 7, 10, 12, 19, 29, 33 ],
 
-  // TC: 2
-  [25, 23, 11, 2, 18, 3, 28, 6, 37],
+	// TC: 2
+	[ 25, 23, 11, 2, 18, 3, 28, 6, 37 ],
 
-  // TC: 3
-  [3, 37, 5, 36, 6, 22, 19, 2, 28],
+	// TC: 3
+	[ 3, 37, 5, 36, 6, 22, 19, 2, 28 ]
 ];
 
 for (let i = 0; i < input.length; i++) {
-  process.stdout.write(`#${i + 1} `);
-  console.log(answer(input[i]));
+	process.stdout.write(`#${i + 1} `);
+	console.log(answer(input[i]));
 }
