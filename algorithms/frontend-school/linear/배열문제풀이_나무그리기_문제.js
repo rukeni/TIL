8e8432e8ec7a4f6d31878e45417e700e3561d039
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-04-09 00:39:29 +0900

    Add : TwoSum 문제풀이

---
+++ algorithms/frontend-school/linear/배열문제풀이_나무그리기_문제.js
@@ -0,0 +1,30 @@
+/* 나무 그리기 */
+
+/* user code */
+function answer(height) {
+	const maxRow = height * 2 - 1;
+	for (let i = 0; i < height; i++) {
+    const centerChar = (i + 1)
+    for(let j = 0; j < maxRow; j++) {
+
+    }
+  }
+
+	return str;
+}
+
+/* main code */
+let input = [
+	// TC: 1
+	3,
+
+	// TC: 2
+	5,
+
+	// TC: 3
+	7,
+];
+
+for (let i = 0; i < input.length; i++) {
+	console.log(`#${i + 1} ${answer(input[i])}`);
+}
