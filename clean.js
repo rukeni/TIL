72161686d5adb6996a50cfa3d3868317e5766049
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-04-13 10:16:34 +0900

    Chore : 현재 프로젝트만 탐색

--- clean.js
+++ clean.js
@@ -6,7 +6,7 @@ const MARKDOWN_PATTERN = '**/*.md';
 
 //Find all md files within a project and ignore node_modules
 const files = glob.sync(MARKDOWN_PATTERN, {
-  cwd: path.resolve(__dirname, '../'),
+  cwd: path.resolve(__dirname),
   ignore: 'node_modules/**'
 });
 
