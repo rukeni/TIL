00fd8c3ceef30bf02b189b39c73910ff8fa140b0
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-04-28 23:39:40 +0900

    Add : typeorm 연동 실습

---
+++ nestjs/the-complete-developer-guide/section7/mycv/src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column() email: string;
+
+  @Column() password: string;
+}
