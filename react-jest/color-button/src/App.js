9e13a267c1742ab3bf4fa4eeaf39dbfcd3980470
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-05-18 23:53:16 +0900

    Test: 버튼 클릭 코드 추가

--- react-jest/color-button/src/App.js
+++ react-jest/color-button/src/App.js
@@ -1,23 +1,14 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{backgroundColor: buttonColor}} onClick={ () => setButtonColor('blue')}>
+        {buttonColor === 'red' ? 'Change to blue' : 'Change to red'}
+      </button>
     </div>
   );
 }
